331b5aaafa455e46830c3d17458d59cbd5137801
PrathamSinghSaluja <prathamsinghsaluja@gmail.com>
2022-04-04 22:34:09 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var myNav = document.getElementById('mynav');
+window.onscroll = function () { 
+    "use strict";
+    if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    } 
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    }
+};
+
+
+document.getElementById("year").innerHTML=new Date().getFullYear();
+
+
+
